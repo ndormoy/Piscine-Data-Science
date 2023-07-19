45f1a49fe94adfcce21c32d5f7f6821239531afd
Noe Dormoy <noedormoy@macbook-pro-de-noe.home>
2023-07-19 14:36:22 +0200

    Day 0 ex02 OK

--- Data Science - 0/ex02/table.py
+++ Data Science - 0/ex02/table.py
@@ -1,11 +1,11 @@
-import psycopg2 as psy
 import pandas as pd
+import sys
 import os
-import sqlalchemy
 from sqlalchemy import create_engine
 
 
 def load(path: str) -> pd.DataFrame:
+    """Load a csv file from path"""
     try:
         if not isinstance(path, str):
             return None
@@ -21,33 +21,48 @@ def load(path: str) -> pd.DataFrame:
     return df
 
 
-def main():
+def create_datatable(path, filename) -> None:
+    """Create a datatable in postgres"""
     try:
-        #interact with PostgreSQL databases.
-        # connection = psy.connect(
-        #     host="localhost",
-        #     port="5432",
-        #     database="piscineds",
-        #     user="ndormoy",
-        #     password="mysecretpassword"
-        # )
-        #The cursor object acts as a handle to the database
-        #and allows you to execute SQL statements on it.
-        # cursor = connection.cursor()
-        df = load("../subject/customer/data_2022_oct.csv")
-        table_name = "data_2022_oct"
-        columns = ", ".join(df.columns)
-        #create a new database engine object
-        engine = create_engine('postgresql://ndormoy:mysecretpassword@localhost:5432/piscineds')
-        #write the DataFrame df into the data_2022_oct... table
+        file_name_without_extension = os.path.splitext(filename)[0]
+        print(file_name_without_extension)
+        df = load(path)
+        table_name = file_name_without_extension
+        # create a new database engine object
+        engine = create_engine(
+            'postgresql://ndormoy:mysecretpassword@localhost:5432/piscineds')
+        # write the DataFrame df into the data_2022_oct... table
         df.to_sql(table_name, engine, if_exists='replace', index=False)
-        # cursor.close()
-        # connection.close()
-        
-    except psy.Error as e:
+    except AssertionError as e:
+        print(e)
+
+
+def main():
+    """Main function"""
+    assert (len(sys.argv) != 1), "You have to pass the path of the file"
+    try:
+        path = sys.argv[1]
+        print(path)
+        absolute_path = os.path.abspath(path)
+        # Get the parent directory of the file (one level up)
+        parent_dir = os.path.dirname(absolute_path)
+        # transform path to filename
+        filename = os.path.basename(path)
+        # Check if the parent directory contains "customer" folder
+        if "customer" in os.path.split(parent_dir)[-1]:
+            print("in customer")
+            if path.lower().endswith('.csv'):
+                create_datatable(path, filename)
+            else:
+                raise ValueError("The file must be a .csv")
+        else:
+            raise ValueError("The parent directory must be --> customer")
+
+    except AssertionError as e:
         print("Error connecting to the PostgreSQL database:", e)
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == '__main__':
     main()
-    
