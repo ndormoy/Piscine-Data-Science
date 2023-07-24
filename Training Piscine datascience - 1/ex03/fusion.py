5c2f04451c60de7f27d0a18c986a99add2a7b6ad
Noe Dormoy <noedormoy@macbook-pro-de-noe.home>
2023-07-24 16:45:30 +0200

    Day01 EX03 OK, DAY1 OK

---
+++ Training Piscine datascience - 1/ex03/fusion.py
@@ -0,0 +1,43 @@
+import csv
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def insert_data_from_csv():
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    db = os.getenv("POSTGRES_DB")
+    port = os.getenv("POSTGRES_PORT")
+    # Connect to the database
+    conn = psycopg2.connect(
+        host="localhost",
+        database=db,
+        user=user,
+        password=password,
+        port=port
+    )
+    cursor = conn.cursor()
+
+    try:
+        # Open the CSV file and copy its contents to the customers table
+        with open("items/data_2023_feb.csv", 'r') as csvfile:
+            cursor.copy_from(csvfile, 'customers', sep=',')
+        # Commit the changes
+        conn.commit()
+
+        print("Data inserted from CSV successfully.")
+    except Exception as e:
+        print("Error:", e)
+        conn.rollback()
+    finally:
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    insert_data_from_csv()
